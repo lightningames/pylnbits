9bdc30be4a1f103ac5ea061339525dc1708e57aa
Brian Liotti <bliotti@protonmail.com>
2021-06-03 00:58:27 -0400

    noqa ignores linter

--- tests/test_userwallet.py
+++ tests/test_userwallet.py
@@ -31,14 +31,8 @@ async def main():
         # res = await uw.create_invoice(False, 150, "testcreatetwo", "http://google.com")
         # print(f'\nCreate invoice RESPONSE: {res}\n\n')
 
-        # pay an invoice - add balance and check
-        bolt = (
-            "lnbc800n1ps23r2dpp5ahh77dmx2d6yd3cw2xheks20kwe3nwh4zhcllxzjcq5fat3kvkssdqsd9h8vmmfvdjk7mn9cqzpgrzjq02sn"
-            "zwz4petaly54yzjkm358rqa5as9hkgydjvxxmvlpuk6dfd9cz0y2cqq0qsqqyqqqqlgqqqqqqgq9qsp5cut63ftfcffwkrr2w9r50w5e40m93k3er"
-            "75mc70ysxps7yercs9s9qyyssqs7qk3cz97nm5m6ehzedcxhttx87l7x5kk38gvwkzzv4lhrhddtqq3sk43nnvsddagf36ledw9vhlpqxuu5s53pj6sz"
-            "926mwqxf8chsgp2m9j8w"
-        )
-
+        # pay an invoice - add balance and check\
+        bolt = "lnbc800n1ps23r2dpp5ahh77dmx2d6yd3cw2xheks20kwe3nwh4zhcllxzjcq5fat3kvkssdqsd9h8vmmfvdjk7mn9cqzpgrzjq02snzwz4petaly54yzjkm358rqa5as9hkgydjvxxmvlpuk6dfd9cz0y2cqq0qsqqyqqqqlgqqqqqqgq9qsp5cut63ftfcffwkrr2w9r50w5e40m93k3er75mc70ysxps7yercs9s9qyyssqs7qk3cz97nm5m6ehzedcxhttx87l7x5kk38gvwkzzv4lhrhddtqq3sk43nnvsddagf36ledw9vhlpqxuu5s53pj6sz926mwqxf8chsgp2m9j8w"  # noqa
         # body = {"out": True, "bolt11": bolt}
         res = await uw.pay_invoice(True, bolt)
         print(res)
