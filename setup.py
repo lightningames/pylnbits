c4ba5d3d05d4c2b94c4b7c7d0eeb4331baf178bc
Bitcarrot <73979971+bitkarrot@users.noreply.github.com>
2021-06-13 16:33:17 -0700

    add setup tools for pypi

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
 from setuptools import find_packages, setup
 
-setup(name="pylnbits", version="1.0", packages=find_packages())
+setup(
+    name="pylnbits-LNGAMES",
+    version="0.0.1",
+    package_dir={"": "pylnbits"},
+    packages=find_packages(where="pylnbits"), 
+    python_requires=">=3.8",)
