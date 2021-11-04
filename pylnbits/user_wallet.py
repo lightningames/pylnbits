32f9aaf4c884ed1cc429db5ce68edcb0a17dc31f
Bitcarrot <73979971+bitkarrot@users.noreply.github.com>
2021-11-03 23:34:37 -0700

    add decode and get invoice methods

--- pylnbits/user_wallet.py
+++ pylnbits/user_wallet.py
@@ -36,6 +36,9 @@ class UserWallet:
         self._headers = config.headers()
         self._admin_headers = config.admin_headers()
         self._lnbits_url = config.lnbits_url
+        self.paypath = "/api/v1/payments"
+        self.walletpath = "/api/v1/wallet"
+
 
     async def get_wallet_details(self):
         """
@@ -48,7 +51,8 @@ class UserWallet:
         {"id": <string>, "name": <string>, "balance": <int>}
         """
         try:
-            upath = "/api/v1/wallet"
+            # upath = "/api/v1/wallet"
+            upath = self.walletpath
             path = self._lnbits_url + upath
             res = await get_url(self._session, path=path, headers=self._headers)
             return res
@@ -69,13 +73,15 @@ class UserWallet:
         {"paid": <bool>}
         """
         try:
-            upath = "/api/v1/payments/" + hash
+            # ppath = "/api/v1/payments/"
+            upath = self.paypath + hash
             path = self._lnbits_url + upath
             res = await get_url(self._session, path=path, headers=self._headers)
             return res
         except Exception as e:
             logger.info(e)
             return e
+
 
     async def create_invoice(self, direction: bool, amt: int, memo: str, webhook: str):
         """
@@ -99,8 +105,8 @@ class UserWallet:
         """
 
         try:
-            upath = "/api/v1/payments"
-            path = self._lnbits_url + upath
+            # upath = "/api/v1/payments"
+            path = self._lnbits_url + self.paypath
             body = {"out": direction, "amount": amt, "memo": memo, "webhook": webhook}
             j = json.dumps(body)
             res = await post_url(self._session, path=path, headers=self._headers, body=j)
@@ -108,6 +114,7 @@ class UserWallet:
         except Exception as e:
             logger.info(e)
             return e
+
 
     async def pay_invoice(self, direction: bool, bolt11: str):
         """
@@ -130,8 +137,8 @@ class UserWallet:
         -H "Content-type: application/json"
         """
         try:
-            upath = "/api/v1/payments"
-            path = self._lnbits_url + upath
+            # upath = "/api/v1/payments"
+            path = self._lnbits_url + self.paypath
             body = {"out": direction, "bolt11": bolt11}
             j = json.dumps(body)
             print(f"body: {j}")
@@ -141,11 +148,144 @@ class UserWallet:
             logger.info(e)
             return e
 
+
+    def get_payurl(self, email: str) -> str:
+        """
+        Construct Lnurlp link from email address provided. 
+        """
+        try:
+            parts = email.split('@')
+            domain = parts[1]
+            username = parts[0]
+            transform_url = "http://" + domain + "/.well-known/lnurlp/" + username
+            print("Transformed URl: "  + transform_url)
+            return transform_url
+        except Exception as e: 
+            print("Exception, possibly malformed LN Address: " + e)
+
+
+    async def get_bolt11(self, email: str, amount: int): 
+        '''
+            fail state
+            {'reason': 'Amount 100 is smaller than minimum 100000.', 'status': 'ERROR'}
+
+            success state
+            {'pr': 'lnbc1......azgfe0', 
+            'routes': [], 'successAction': {'description': 'Thanks love for the lightning!', 
+            'tag': 'url', 'url': 'https:/.......'}}
+        '''
+        try: 
+            purl = self.get_payurl(email)
+            res = await get_url(self._session, path=purl)
+            # res =  requests.get(purl)
+            json_content = res.json()
+            lnurlpay = json_content['callback']
+
+            millisats = amount * 1000
+            payquery = lnurlpay + "?amount=" + str(millisats)
+
+            # get bech32-serialized lightning invoice
+            # ln_res =  requests.get(payquery)
+            ln_res = await get_url(self._session, path=payquery)
+            pr_dict = ln_res.json()
+            # check keys returned for status
+            if 'status' in pr_dict: 
+                reason = pr_dict['reason']
+                return reason
+            elif 'pr' in pr_dict: 
+                bolt11 = pr_dict['pr']
+                return bolt11
+        except Exception as e: 
+            print("Exception as: " + e)
+            return e
+
+
+    async def get_decoded(self, bolt11: str):
+        """
+        POST /api/v1/payments/decode
+
+        Body (application/json)
+        {"data": <string>}
+
+        Returns 200 (application/json) 
+        
+        Headers: invoice key
+        """
+        try:
+            decode_url = self._lnbits_url + self.paypath + "/decode"
+            payload = {'data': bolt11}
+            # res =  requests.post(decode_url, json=payload, headers=self._headers)
+            res = await post_url(self._session, path=decode_url, body=payload, headers=self._headers)
+            return res
+        except Exception as e:
+            print('Exception in get_decoded() ', e)
+            return e
+
+
+    # get payment hash from bolt11
+    async def get_payhash(self, bolt11: str):
+        """
+        Only returns the payment hash not entire decoded invoice
+        """
+        res = await self.get_decoded(bolt11)
+        decoded = res.json()
+        # print(decoded)
+        if 'payment_hash' in decoded:
+            payhash = decoded['payment_hash']
+            return payhash
+        else:
+            return None
+
+
+    async def get_invoices(self):
+        """
+        Get invoices (incoming or outgoing)
+
+        GET /api/v1/payments
+
+        Headers: invoice key
+
+        Returns 200 OK (application/json)
+        [{<invoices>}]
+        """
+        try:
+            # ppath = "/api/v1/payments/"
+            path = self._lnbits_url + self.paypath
+            res = await get_url(self._session, path=path, headers=self._headers)
+            return res
+        except Exception as e:
+            logger.info(e)
+            return e
+
+
+    async def get_invoicesbymemo(self, memo: str):
+        """
+        GET /api/v1/payments?memo=<memo>
+
+        Returns 200 OK (application/json)
+        [{<invoices>}]
+
+        Headers: invoice key
+        """
+        try:
+            path = self._lnbits_url + self.paypath + "?memo=" + memo
+            res = await get_url(self._session, path=path, headers=self._headers)
+            return res
+        except Exception as e:
+            logger.info(e)
+            return e
+
+
+
+
 """
  todo: 
  
- decode an invoice
- Get invoices (incoming or out)
- Get invoice(s) by memo (incoming or out)
+ decode an invoice - done
+
+ Get invoices (incoming or out) - todo - test!
+ Get invoice(s) by memo (incoming or out) - todo - test!
+
+ add return types and check
 """
  
