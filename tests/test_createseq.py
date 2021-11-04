54deac1c55ad6c1ce014deb12ce897693b93e46d
Bitcarrot <73979971+bitkarrot@users.noreply.github.com>
2021-11-03 22:40:07 -0700

    update test

--- tests/test_createseq.py
+++ tests/test_createseq.py
@@ -1,10 +1,28 @@
 '''
- sequence :
+test sequence :
+ 
+1. create a user with user manager w/initial wallet.
+   save user id, admin key, invoice key to db, connect data w/ TG username
+2. activate lnurlp and lnurlw on new user
+3. activate lndhub extension
 
- 1. create user with user manager w/initial wallet.
-    save userid, admin, invoice to db, connect data w/username
- 2. activate lnurlp on new user
- 3. activate lndhub extension
+store data via supabase
+
+- receive by 
+      - bolt11 invoice
+      - lnurlp link
+      - laisee send
+- send by 
+      - lnurlw link (laisee)
+      - LN address: user@domain.com
+      - laisee send. 
+
+
+- export to wallet
+- check balance
+
+
+==========================================
 
 Telegram comands for: 
 
@@ -12,7 +30,7 @@ match telegram  user id with keys for
  1. get wallet balance "get wallet details" from main wallet
  2. get wallet transactions (from user manager) 
 
- pay another telegram user - check db for record
+ pay another telegram user - 
 
  3. /send - create invoice on receiving user, then pay an invoice (outgoing) from main wallet
     - enable /send <amt> @username on telegram
@@ -20,7 +38,6 @@ match telegram  user id with keys for
  4. /receive - create an invoice (incoming) from main wallet - 
     /receive (lnurl | (any | <satoshis>) [<description>...])
     Generates a BOLT11 invoice with given satoshi value.
-
 
  5. /balance  - gets wallet balance and txs
    /transactions
