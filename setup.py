9a5202605d95aaf220a293b9d2f974a6ab6eb52d
Bitcarrot <73979971+bitkarrot@users.noreply.github.com>
2021-11-27 01:58:58 -0800

    update toml and setup

--- setup.py
+++ setup.py
@@ -1,39 +1,6 @@
-from setuptools import find_packages, setup
+#!/usr/bin/env python
 
-setup(
-    name="pylnbits-LNGAMES",
-    version="0.0.3",
-    package_dir={"": "pylnbits"},
-    packages=find_packages(where="pylnbits"),
-    python_requires=">=3.8",
-)
+import setuptools
 
-VERSION = "0.0.3"
-
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-install_requires = open("requirements.txt").readlines()
-
-setup(
-    name="pylnbits-lngames",
-    version=VERSION,
-    author="bitkarrot",
-    description="python library for lnbits",
-    maintainer="Lightning Games",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/lightningames/pylnbits",
-    project_urls={
-        "Bug Tracker": "https://github.com/lightningames/pylnbits/issues",
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    packages=find_packages(),
-    install_requires=install_requires,
-    include_package_data=True,
-    python_requires=">=3.8",
-)
+if __name__ == "__main__":
+    setuptools.setup()
