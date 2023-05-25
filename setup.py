8b86a893b2639a61ed4243a18845ec1b31a0aabf
Bitkarrot <73979971+bitkarrot@users.noreply.github.com>
2023-05-24 23:05:14 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,34 @@
 #!/usr/bin/env python
-
-import setuptools
+from setuptools import find_packages, setup
 
 if __name__ == "__main__":
-    setuptools.setup()
+    # setuptools.setup()
+    VERSION = "0.0.6"
+
+    with open("README.md", "r", encoding="utf-8") as fh:
+        long_description = fh.read()
+
+    install_requires = open("requirements.txt").readlines()
+
+    setup(
+        name="pylnbits",
+        version=VERSION,
+        author="bitkarrot",
+        description="python library for lnbits",
+        maintainer="Lightning Games",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/lightningames/pylnbits",
+        project_urls={
+            "Bug Tracker": "https://github.com/lightningames/pylnbits/issues",
+        },
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+        packages=find_packages(),
+        install_requires=install_requires,
+        include_package_data=True,
+        python_requires=">=3.8",
+    )
