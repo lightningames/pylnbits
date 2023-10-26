83ba0acbfcbde31e9410de654b4849d874e708e9
Fenix <fenixlibertas@gmail.com>
2023-10-26 14:50:24 +0200

    Invice extension tests updated

--- tests/test_invoices.py
+++ tests/test_invoices.py
@@ -18,18 +18,8 @@ async def main():
         inv = Invoices(c, session)
         print(f"\n")
 
-        # get invoices
-        invoices = await inv.get_invoices()
-        print(f"Invoices:\n{invoices}\n")
-
-        # get invoice
-        # update the invoice_id for your test case
-        invoice_id = "8vYb8ui5fpEa6ELKoHt6CB"
-        res = await inv.get_invoice(invoice_id)
-        print(f"Invoice:\n{res}\n")
-
         # create invoice
-        invoice_dto = InvoiceDTO("wallet", "EUR", "draft")
+        invoice_dto = InvoiceDTO("wallet", "EUR", "open")
         invoice_dto.company_name = 'my_company_name10'
         invoice_dto.first_name = 'my_first_name'
         invoice_dto.last_name = 'my_last_name'
@@ -40,10 +30,29 @@ async def main():
         res = await inv.create_invoice(invoice_dto)
         print(f"Created invoice \n{res}\n")
 
+        
+        invoice_id = res['id']
+        # get invoices
+        invoices = await inv.get_invoices()
+        print(f"Invoices:\n{invoices}\n")
+
+        # get invoice
+        res = await inv.get_invoice(invoice_id)
+        print(f"Invoice:\n{res}\n")
+
+        # create invoice payment
+        res = await inv.create_invoice_payment(invoice_id)
+        print(f"Invoice payment::\n{res}\n")
+
+        payment_hash = res['payment_hash']
+        # check invoice payment status
+        res = await inv.get_invoice_payment_status(invoice_id, payment_hash)
+        print(f"Invoice payment status:\n{res}\n")
+
         # update invoice
         # update the invoice_id for your test case
         invoice_dto = InvoiceDTO("wallet", "EUR", "draft")
-        invoice_dto.id = 'NWQt8WbEXLLBP5kR7v25tD'
+        invoice_dto.id = invoice_id
         invoice_dto.company_name = 'my_company_name10_1'
         invoice_dto.first_name = 'my_first_name1'
         invoice_dto.last_name = 'my_last_name1'
@@ -54,25 +63,13 @@ async def main():
         res = await inv.update_invoice(invoice_dto)
         print(f"Updated invoice: \n{res}\n")
 
-        # create invoice payment
-        # update the invoice_id for your test case
-        invoice_id = "oKHncemJR72iWjbMWuWdXm"
-        res = await inv.create_invoice_payment(invoice_id)
-        print(f"Invoice payment::\n{res}\n")
-
-        # check invoice payment status
-        # update the invoice_id and payment_hash for your test case
-        invoice_id = "oKHncemJR72iWjbMWuWdXm"
-        payment_hash = "1c4643d0f2be78e82556c4cddb1723d20d7bda3f9e2c591b00a92baf9b4992f1"
-        res = await inv.get_invoice_payment_status(invoice_id, payment_hash)
-        print(f"Invoice payment status:\n{res}\n")
-
-        # delete invoice
-        # update the invoice_id for your test case
-        invoice_id = "CoxjMMnGKtjCyqAMv37rFK"
+        #delete invoice
         res = await inv.delete_invoice(invoice_id)
         print(f"Delete Invoice result::\n{res}\n")
 
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
+
+#DeprecationWarning: There is no current event loop
+#  loop = asyncio.get_event_loop()
