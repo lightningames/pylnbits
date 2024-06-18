678face07407dba717a5e9f08a06b0c8c138bd2b
Judy Imasuen <imasuenjudy@gmail.com>
2024-06-18 17:56:12 +0100

    Added support for Split Payments extension with test cases

--- pylnbits/split_payments.py
+++ pylnbits/split_payments.py
@@ -3,7 +3,7 @@ import logging
 
 from aiohttp.client import ClientSession
 
-from pylnbits.utils import delete_url, get_url, post_url
+from pylnbits.utils import delete_url, get_url, put_url
 
 """
 Rest API methods for LNbits Split Payments Extension
@@ -51,6 +51,62 @@ class SplitPayments:
             upath = self.splitpath
             path = self._lnbits_url + upath
             res = await get_url(self._session, path=path, headers=self._admin_headers)
+            return res
+        except Exception as e:
+            logger.info(e)
+            return e
+    
+    # Add target wallet
+    async def add_target_wallet(self, wallet: str, alias: str, percent: float):
+        """
+        PUT /splitpayments/api/v1/targets
+
+        Headers
+        {"X-Api-Key": "Admin key"}
+
+        Returns null 
+        200 OK (application/json)
+        
+        `wallet` can either be a lightning address, lnurl, lnbits internal wallet id.
+        `percent` can up to 6 decimal places
+        """
+        try:
+            upath = self.splitpath
+            path = self._lnbits_url + upath
+
+            # check if there are other targets to avoid overwriting
+            targets = await self.get_target_wallets()
+            data = [{"wallet": wallet, "alias" : alias, "percent": percent}]
+            if targets is not None:
+                data = targets + data
+            body = {"targets": data}
+            j = json.dumps(body)
+            res = await put_url(self._session, path=path, headers=self._admin_headers, body=j)
+            
+            # response body is null when successful. Return the updated list
+            if res is None:
+                return body
+            return res
+        except Exception as e:
+            logger.info(e)
+            return e
+        
+    # Delete all target wallets
+    async def delete_target_wallets(self):
+        """
+        DELETE /splitpayments/api/v1/targets
+
+        Headers
+        {"X-Api-Key": "Admin key"}
+
+        Returns null 
+        200 OK
+        """
+        try:
+            upath = self.splitpath
+            path = self._lnbits_url + upath
+            res = await delete_url(self._session, path=path, headers=self._admin_headers)
+            # response body is null when successful.
             return res
         except Exception as e:
             logger.info(e)
--- tests/test_splitpayments.py
+++ tests/test_splitpayments.py
@@ -15,9 +15,19 @@ async def main():
 
     async with ClientSession() as session:
         sp = SplitPayments(c, session)
-        # works
-        targetwallets = await sp.get_target_wallets()
-        print(f"target wallets : {targetwallets}")
+
+        # get list of target wallets
+        # targetwallets = await sp.get_target_wallets()
+        # print(f"Target wallets : {targetwallets}")
+
+        # add target wallets
+        # addwallets = await sp.add_target_wallet("me@example.com", "Me", 50)
+        # print(f"Updated list of target wallets: {addwallets}")
+
+        # delete list of target wallets
+        deletewallets = await sp.delete_target_wallets()
+        print(f"status: {deletewallets}")
+
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
